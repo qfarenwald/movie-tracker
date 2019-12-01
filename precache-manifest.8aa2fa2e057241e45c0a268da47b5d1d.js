1207ea7fb62c3f4281834f21f78b8d07c66d3dbb
Quinne Farenwald <q.farenwald@gmail.com>
2019-12-01 13:00:15 -0700

    Updates

---
+++ precache-manifest.8aa2fa2e057241e45c0a268da47b5d1d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eeacc22d42cb100a5114f8f06b19aa1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9249f7c226d2c5e81385",
+    "url": "./static/css/main.9dc86d5f.chunk.css"
+  },
+  {
+    "revision": "c918d36ddeccac2cfc50",
+    "url": "./static/js/2.9513c7fa.chunk.js"
+  },
+  {
+    "revision": "9249f7c226d2c5e81385",
+    "url": "./static/js/main.ec3ec32c.chunk.js"
+  },
+  {
+    "revision": "f513f22a11637feaa461",
+    "url": "./static/js/runtime-main.df0b2151.js"
+  },
+  {
+    "revision": "6757b8fe7adb82f07d7947ec3e3868f3",
+    "url": "./static/media/MovieTracker_star.6757b8fe.svg"
+  },
+  {
+    "revision": "50697aa654e2ae4dcad219b6e8220ad5",
+    "url": "./static/media/MovieTracker_star_fav.50697aa6.svg"
+  },
+  {
+    "revision": "9d6b03227c9208561d4d20dc0c1931c0",
+    "url": "./static/media/background-curtain.9d6b0322.jpg"
+  },
+  {
+    "revision": "00514d8f3c427649a27261ebec05596f",
+    "url": "./static/media/red-curtain-&-empty-theatre.00514d8f.jpg"
+  }
+]);
